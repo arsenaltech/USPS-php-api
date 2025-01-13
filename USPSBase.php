3c9d79d5101ddac20328927e4cefb0363bed5eed
Chirag <chirag.prajapati@arsenaltech.com>
2025-01-13 14:47:42 +0530

    Updated the Base url for USPS (#2)

--- USPSBase.php
+++ USPSBase.php
@@ -10,7 +10,7 @@ require_once('XMLParser.php');
  * @author Vincent Gabriel
  */
 class USPSBase {
-    const LIVE_API_URL = 'http://production.shippingapis.com/ShippingAPI.dll';
+    const LIVE_API_URL = 'https://production.shippingapis.com/ShippingAPI.dll';
     const TEST_API_URL = 'http://production.shippingapis.com/ShippingAPITest.dll';
 
   /**
